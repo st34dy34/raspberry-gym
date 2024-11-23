092f9a1c7fa94ccc74a2ee4ebf2e795c42dbb0a1
steady7 <hruskadave01@gmail.com>
2024-11-23 14:38:11 +0100

    finished displaying member list

--- Membership/views.py
+++ Membership/views.py
@@ -4,7 +4,11 @@ from .models import Member
 # Create your views here.
 
 def member_list(request):
-    members = Member.objects.all()
+    membership_type = request.GET.get('membership_type')  # Get the filter parameter
+    if membership_type:
+         members = Member.objects.filter(membership_type=membership_type)
+    else:
+        members = Member.objects.all()
     context = {
         'members':members,
     }
